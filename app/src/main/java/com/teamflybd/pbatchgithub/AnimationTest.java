fc0dc7fff0733617d247768cd0f06650b704ef24
Adil <https://_redemption@bitbucket.org/_redemption/lu-canteen.git>
2018-11-21 11:33:59 +0600

    Animation(zoomIn, zoomOut, fadeIn, fadeOut, blink) implemented

---
+++ app/src/main/java/com/teamflybd/pbatchgithub/AnimationTest.java
@@ -0,0 +1,84 @@
+package com.teamflybd.pbatchgithub;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class AnimationTest extends AppCompatActivity implements View.OnClickListener{
+    private Button bZoomIn, bZoomOut, bBlink, bFadeIn, bFadeOut, bCrossfade;
+
+    private Animation aZoomIn, aZoomOut, aBlink, aFadeIn, aFadeOut, aCrossfadeIn, aCrossfadeOut;
+
+    private TextView textView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_animation_test);
+
+        bZoomIn = findViewById(R.id.zoom_in);
+        bZoomOut = findViewById(R.id.zoom_out);
+        bBlink = findViewById(R.id.blink);
+        bFadeIn = findViewById(R.id.fade_in);
+        bFadeOut = findViewById(R.id.fade_out);
+        bCrossfade = findViewById(R.id.cross_fade);
+
+        textView = findViewById(R.id.test);
+
+        //add listener
+        bZoomIn.setOnClickListener(this);
+        bZoomOut.setOnClickListener(this);
+        bBlink.setOnClickListener(this);
+        bFadeIn.setOnClickListener(this);
+        bFadeOut.setOnClickListener(this);
+        bCrossfade.setOnClickListener(this);
+
+        aZoomIn = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.zoom_in);
+        aZoomOut = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.zoom_out);
+        aBlink = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.blink);
+        aFadeIn = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.fade_in);
+        aFadeOut = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.fade_out);
+        aCrossfadeIn = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.fade_in);
+        aCrossfadeOut = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.fade_out);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId())
+        {
+            case R.id.zoom_in :
+                textView.setVisibility(View.VISIBLE);
+                textView.startAnimation(aZoomIn);
+                break;
+
+            case R.id.zoom_out :
+                textView.setVisibility(View.VISIBLE);
+                textView.startAnimation(aZoomOut);
+                break;
+
+            case R.id.blink :
+                textView.setVisibility(View.VISIBLE);
+                textView.startAnimation(aBlink);
+                break;
+
+            case R.id.fade_in :
+                textView.setVisibility(View.VISIBLE);
+                textView.startAnimation(aFadeIn);
+                break;
+
+            case R.id.fade_out :
+                textView.setVisibility(View.VISIBLE);
+                textView.startAnimation(aFadeOut);
+                break;
+
+            case R.id.cross_fade :
+                textView.setVisibility(View.VISIBLE);
+                textView.startAnimation(aCrossfadeIn);
+                textView.startAnimation(aCrossfadeOut);
+                break;
+        }
+    }
+}
