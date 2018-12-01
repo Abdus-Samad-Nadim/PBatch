c7f74c27884eacb9e27c7f848be5fd19fe983fea
Adil <https://_redemption@bitbucket.org/_redemption/lu-canteen.git>
2018-12-01 14:59:58 +0600

    TextToSpeech and SpeechToText implemented

---
+++ app/src/main/java/com/teamflybd/pbatchgithub/SpeechToTextTest.java
@@ -0,0 +1,67 @@
+package com.teamflybd.pbatchgithub;
+
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.speech.RecognizerIntent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Locale;
+
+public class SpeechToTextTest extends AppCompatActivity {
+    TextView text;
+    Button mic;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_speech_to_text_test);
+        initialization();
+    }
+
+    private void initialization() {
+        text = findViewById(R.id.text);
+        mic = findViewById(R.id.mic);
+
+        mic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getVoiceInput();
+            }
+        });
+    }
+
+    private void getVoiceInput() {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Please speak something");
+
+        try{
+            startActivityForResult(intent, 100);
+        } catch (ActivityNotFoundException a)
+        {
+
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode){
+            case 100: {
+                if(resultCode == RESULT_OK && null != data)
+                {
+                    ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    text.setText(result.get(0));
+                }
+                break;
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/teamflybd/pbatchgithub/TextToSpeechTest.java
@@ -0,0 +1,78 @@
+package com.teamflybd.pbatchgithub;
+
+import android.os.Build;
+import android.speech.tts.TextToSpeech;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.Locale;
+
+public class TextToSpeechTest extends AppCompatActivity implements TextToSpeech.OnInitListener {
+    EditText editText;
+    Button button;
+    TextToSpeech tts;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_text_to_speech);
+        initialization();
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                voiceOutput();
+            }
+        });
+    }
+
+    private void initialization() {
+        tts = new TextToSpeech(this, this);
+        editText = findViewById(R.id.input);
+        button = findViewById(R.id.click);
+    }
+
+    @Override
+    public void onInit(int status) {
+        if(status == TextToSpeech.SUCCESS)
+        {
+            int result = tts.setLanguage(Locale.US);
+            float i = 50;
+
+            if(result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED)
+            {
+                Toast.makeText(this, "Lanuage not supported", Toast.LENGTH_SHORT).show();
+            }
+            else
+            {
+                button.setEnabled(true);
+                voiceOutput();
+            }
+        }
+        else
+        {
+            Toast.makeText(this, "Initialization failed", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void voiceOutput() {
+        CharSequence text = editText.getText();
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            tts.speak(text, TextToSpeech.QUEUE_FLUSH, null, "id1");
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        if(tts!=null)
+        {
+            tts.stop();
+            tts.shutdown();
+        }
+        super.onDestroy();
+    }
+}
